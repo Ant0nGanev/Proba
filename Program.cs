90e3ea8d5278ad9f410397141720fc22ff1e6af2
Ant0nGanev <ag92472563@edu.mon.bg>
2024-09-30 13:07:20 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Vavedi a");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("a=" + a);
+int b = int.Parse(Console.ReadLine());
+Console.WriteLine("b=" + b);
+int SumaPlus = a + b;
+Console.WriteLine();
+Console.WriteLine("Sumata e " + SumaPlus);
